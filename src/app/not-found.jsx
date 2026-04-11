d2d70f9ea06bc2ce9a3a104f530a075364dfeea8
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-11 18:21:17 +0600

    project setup,  added daisyUI and notFound page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div className='h- h-screen bg-purple-300 flex  items-center justify-center font-bold text-5xl text-black'>
+      this page is not found..!
+    </div>
+  );
+};
+
+export default NotFoundPage;
